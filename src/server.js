58438b8e03a70dd90faa675995e5fb5e2be59b71
rachelumwari <umwarirachel13@gmail.com>
2023-01-16 15:49:14 +0200

    add others change

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(cors())
 app.use(routers)
 
-// const port =process.env.PORT || 8000;
-const port =1000;
+const port =process.env.PORT || 8000;
+// const port =1000;
 app.listen(port, ()=>{
     console.log(`server is read on port http://localhost:${port}`)
     swaggerDocs(app, port);
