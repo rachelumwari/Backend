3f11a8130c765c32cd1c4b71212856c1e43f9ed7
rachelumwari <umwarirachel13@gmail.com>
2023-01-17 14:42:00 +0200

    add other change in the packages json

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 
-const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DATABASE_URL, DB_NAME_TEST } = process.env;
+const DB_USER="postgres", DB_PASSWORD="ZfxkwRrQF508XsRtZROT", DB_NAME="railway", DB_HOST="containers-us-west-197.railway.app", DB_PORT=7385 ;
+
+// const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DATABASE_URL, DB_NAME_TEST } = process.env;
 module.exports = {
 	development: {
-		username:"postgres" ,
-		password: "1234567",
-		database: "users",
-		host: "localhost",
+		username: DB_USER,
+		password: DB_PASSWORD,
+		database: DB_NAME_TEST,
+		host: DB_HOST,
 		dialect: 'postgres',
-		port: 5432,
+		port: DB_PORT,
 		logging: false,
 	},
 	test: {
